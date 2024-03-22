d2f0e54723a4baae713091712bb9f42eb0b9f278
Ismailab1 <ismailabdullah1129@gmail.com>
2024-03-22 16:06:49 -0700

    testing reading bdf files

--- eeg_reading.py
+++ eeg_reading.py
@@ -7,9 +7,12 @@ This is a temporary script file.
 
 import pandas as pd
 import mne
+import os
+import os.path as opo
+import numpy as np
 
 # Step 1: Read the data
-data = pd.read_csv('OpenBCI-R-ARM-M-RAW-2023-12-13_14-49-07.txt', delimiter=',', skiprows=4)
+data = pd.read_csv('OpenBCI-RAW-NEW-HELMET-2024-02-09_16-49-28.txt', delimiter=',', skiprows=4)
 
 # Extract the EEG data and timestamps
 eeg_channels = [' EXG Channel 0', ' EXG Channel 1', ' EXG Channel 2', 
@@ -27,7 +30,17 @@ sfreq = 250  # The sampling frequency of your EEG data
 info = mne.create_info(ch_names=eeg_channels, sfreq=sfreq, ch_types='eeg')
 
 # Step 4: Create the Raw object
-raw = mne.io.RawArray(eeg_data_volts, info)
+raw = mne.io.read_raw_nihon(eeg_data_volts, preload=False, verbose=None)
+raw.load_data()
+
+#Step 5: Start Preprocessing
+raw.set_channel_types({'EOG':'eog'})
+eog_events = mne.preprocessing.find_eog_events(raw)
+onsets = eog_events[:, 0] / raw.info["sfreq"] - 0.25
+durations = [0.5] * len(eog_events)
+descriptions = ["bad blink"] * len(eog_events)
+blink_annot = mne.annotations(onsets, durations, descriptions, orig_time=raw.info["meas_date"])
+raw.set_annotations(blink_annot)
 
 # Step 5: Plot the data
 raw.plot(duration=30, start=0, scalings='auto', block=True)
